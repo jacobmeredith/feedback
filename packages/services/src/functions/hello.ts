f6937be888c44b42fff8d3daeb5ae4ec85da0ad1
Jacob Meredith <jacobmeredith@hotmail.co.uk>
2021-12-27 12:33:54 +0000

    chore: set up dashboard repo

--- packages/services/src/functions/hello.ts
+++ packages/services/src/functions/hello.ts
@@ -1,8 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 
 export const hello = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
-  console.log({event, context});
-
   return {
     statusCode: 200,
     body: JSON.stringify({
