6c8139570406bebd1f94ede269f162496ae3ebb2
Jacob Meredith <jacobmeredith@hotmail.co.uk>
2021-12-29 01:41:42 +0000

    feat: list feedback captures on dashboard

--- packages/dashboard/src/pages/Feedback/Feedback.tsx
+++ packages/dashboard/src/pages/Feedback/Feedback.tsx
@@ -2,9 +2,37 @@ import * as React from 'react';
 
 import {Button, Card, Drawer} from '@feedback/design-system';
 import { FeedbackCreateForm } from '../../components/FeedbackCreateForm';
+import { httpClient } from '../../helpers/httpClient';
 
 function Feedback() {
   const [drawer, setDrawer] = React.useState<null|'create'|'update'|'delete'>(null);
+  const [surveys, setSurveys] = React.useState<Array<any>>([]);
+
+  React.useEffect(() => {
+    httpClient.get('/feedback/1')
+      .then(({data}) => {
+        setSurveys(data);
+      });
+  }, []);
+
+  const surveysMap = surveys.map(survey => (
+    <div className="mb-4">
+      <Card key={survey.type}>
+        <h3 className="text-lg">{survey.feedbackTitle}</h3>
+        <p className="mt-2">
+          URL: <a className="text-blue-700 hover:underline" href={survey.feedbackUrl}>{survey.feedbackUrl}</a>
+        </p>
+        <div className="flex mt-4">
+          <div>
+            <Button type="outline" onClick={() => setDrawer('update')}>Update</Button>
+          </div>
+          <div className="ml-2">
+            <Button type="danger" onClick={() => setDrawer('delete')}>Delete</Button>
+          </div>
+        </div>
+      </Card>
+    </div>
+  ));
 
   return (
     <React.Fragment>
@@ -16,8 +44,8 @@ function Feedback() {
         </React.Fragment>
       </Drawer>
       <Button type="secondary" onClick={() => setDrawer('create')}>Create new survey</Button>
-      <div className="mt-6">
-        <Card>Test</Card>
+      <div className="mt-6 flex flex-col">
+        {surveysMap}
       </div>
     </React.Fragment>
   )
--- packages/design-system/src/components/Button/Button.tsx
+++ packages/design-system/src/components/Button/Button.tsx
@@ -7,12 +7,12 @@ interface IButtonProps {
 }
 
 enum EnumButtonType {
-  primary = 'bg-purple-700 text-white font-bold rounded',
-  secondary = 'bg-gray-300 text-gray-700 rounded',
+  primary = 'bg-purple-700 border-purple-700 text-white font-bold rounded',
+  secondary = 'bg-gray-300 border-gray-300 text-gray-700 rounded',
   outline = 'border-gray-500 text-gray-700 rounded',
-  warning = 'bg-yellow-500 text-white rounded',
-  danger = 'bg-red-500 text-white rounded',
-  success = 'bg-green-500 text-white rounded'
+  warning = 'bg-yellow-500 border-yellow-500 text-white rounded',
+  danger = 'bg-red-500 border-red-500 text-white rounded',
+  success = 'bg-green-500 border-green-500 text-white rounded'
 }
 
 function Button({children, type, onClick = () => {}}: IButtonProps) {
