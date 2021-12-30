0ac51d1cba644f41415211d5efe50d94640741b2
Jacob Meredith <jacobmeredith@hotmail.co.uk>
2021-12-30 03:21:48 +0000

    feat: add option to collapse menu

--- packages/dashboard/src/App.tsx
+++ packages/dashboard/src/App.tsx
@@ -10,10 +10,12 @@ import { Sidebar } from './components/Sidebar';
 import { Feedback } from './pages/Feedback';
 
 function App() {
+  const [collapsed, setCollapsed] = React.useState(false);
+
   return (
     <div className="flex">
-      <Sidebar />
-      <div className="bg-gray-100 w-5/6 h-screen p-6 overflow-auto">
+      <Sidebar collapsed={collapsed} onCollapse={() => setCollapsed(!collapsed)} />
+      <div className={`bg-gray-100 h-screen p-6 overflow-auto ease-in-out duration-300 transition-all ${collapsed ? 'w-full' : 'w-5/6'}`}>
         <Router>
           <Routes>
             <Route path="/feedback" element={<Feedback />} />
--- packages/dashboard/src/components/Sidebar/Sidebar.tsx
+++ packages/dashboard/src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import * as React from 'react';
 
-function Sidebar() {
+interface ISidebarProps {
+  collapsed: boolean;
+  onCollapse: () => void;
+}
+
+function Sidebar({collapsed, onCollapse}: ISidebarProps) {
   return (
-    <aside className="w-1/6 h-screen px-6 py-4 bg-purple-700">
-      
+    <aside className={`h-screen px-6 py-4 bg-purple-700 ease-in-out duration-300 transition-all ${collapsed ? 'w-16' : 'w-1/6'}`}>
+      <button onClick={onCollapse}>{collapsed ? '>' : '<'}</button>
     </aside>
   )
 }
