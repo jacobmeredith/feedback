5317379bfeb9f329dee9f3a9d2a50b082096e5c7
Jake Meredith <jacobmeredith@hotmail.co.uk>
2022-01-13 17:53:19 +0000

    chore: add region to environment variables

--- packages/platform-api/src/data/client.ts
+++ packages/platform-api/src/data/client.ts
@@ -3,7 +3,12 @@ import {DynamoDBClient} from '@aws-sdk/client-dynamodb';
 import Schema from './schema';
 import {Table} from 'dynamodb-onetable';
 
-const client = new Dynamo({client: new DynamoDBClient({ region: 'eu-west-1', endpoint: process.env.TABLE_ENDPOINT })});
+const client = new Dynamo({
+  client: new DynamoDBClient({ 
+    region: process.env.TABLE_REGION,
+    endpoint: process.env.TABLE_ENDPOINT 
+  })
+});
 
 const table = new Table({
   client: client,
